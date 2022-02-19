a246e86bb27878275be2b62a73acf2171389e4e9
Nick Moreton <nickmoreton@me.com>
2022-02-19 15:01:56 +0000

    initial commmit
    update gitignore
    
    remove dist
    
    demo site
    
    save

---
+++ demo/home/models.py
@@ -0,0 +1,44 @@
+from modelcluster.fields import ParentalKey
+from wagtail.admin.edit_handlers import (
+    FieldPanel,
+    FieldRowPanel,
+    InlinePanel,
+    MultiFieldPanel,
+)
+from wagtail.contrib.forms.models import AbstractEmailForm, AbstractFormField
+from wagtail.core.fields import RichTextField
+from wagtail.core.models import Page
+
+from wagtail_honeypot.models import HoneyPotMixin
+
+
+class HomePage(Page):
+    pass
+
+
+class FormField(AbstractFormField):
+    page = ParentalKey("FormPage", related_name="form_fields")
+
+
+class FormPage(HoneyPotMixin):
+    intro = RichTextField(blank=True)
+    thank_you_text = RichTextField(blank=True)
+    # honeypot = models.BooleanField(default=False)
+
+    content_panels = AbstractEmailForm.content_panels + [
+        FieldPanel("intro", classname="full"),
+        InlinePanel("form_fields", label="Form fields"),
+        FieldPanel("thank_you_text", classname="full"),
+        MultiFieldPanel(
+            [
+                FieldRowPanel(
+                    [
+                        FieldPanel("from_address", classname="col6"),
+                        FieldPanel("to_address", classname="col6"),
+                    ]
+                ),
+                FieldPanel("subject"),
+            ],
+            "Email",
+        ),
+    ]
