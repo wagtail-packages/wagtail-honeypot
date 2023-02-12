681d876024ed0d7f203c82fa3ecdd849fc70a5d2
Nick Moreton <nickmoreton@me.com>
2023-02-12 13:25:16 +0000

    Add mysql testing

--- tests/settings.py
+++ tests/settings.py
@@ -102,6 +102,18 @@ if os.getenv("DATABASE") == "postgres":
         }
     }
 
+if os.getenv("DATABASE") == "mysql":
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.mysql",
+            "NAME": os.getenv("DB_NAME", "mysql"),
+            "USER": os.getenv("DB_USER", "root"),
+            "PASSWORD": os.getenv("DB_PASSWORD", "mysql"),
+            "HOST": os.getenv("DB_HOST", "127.0.0.1"),
+            "PORT": os.getenv("DB_PORT", "3306"),
+        }
+    }
+
 
 AUTH_PASSWORD_VALIDATORS = [
     {
